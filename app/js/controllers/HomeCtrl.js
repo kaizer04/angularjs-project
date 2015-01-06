30004fe87ef8d312543f5ef093ba9b743b6d4b36
kaizer04 <kaizer0505@gmail.com>
2015-01-06 20:00:55 +0200

    start again

--- app/js/controllers/HomeCtrl.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-app.controller('HomeCtrl', ['$scope', 'StatsResource', 'TripsResource', 'DriversResource',
-    function HomeCtrl($scope, TripsResource, DriversResource) {
-//        $scope.stats = StatsResource.get();
-//        $scope.trips = TripsResource.public();
-//        $scope.drivers = DriversResource.public();
-//        $scope.hideIsMine = true;
-    }]);
