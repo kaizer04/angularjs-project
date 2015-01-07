cfd88ca075a233fb09e5caa3846c68a5e280f021
kaizer04 <kaizer0505@gmail.com>
2015-01-07 20:15:36 +0200

    config restful services in adsData

--- app/js/services/data/adsData.js
+++ app/js/services/data/adsData.js
@@ -1,1 +1,33 @@
+app.factory('adsData', 'baseServiceUrl', ['$resource', function($resource, baseServiceUrl){
+    var resource = $resource(baseServiceUrl + 'ads:adId', {adId: '@id'}, {
+        update: {method: 'PUT'}
+    });
 
+    function getPublicAds(){ //pageSize, startPage
+        return resource.query();
+    };
+
+    function editAd (adId, ad) {
+        return resource.update({id: adId}, ad);
+    };
+
+    function getAdById (adId) {
+        return resource.get({id: adId});
+    };
+
+    function addAd (ad) {
+        return resource.save(ad);
+    };
+
+    function deleteAd (adId) {
+        return resource.delete({id: adId});
+    };
+
+    return {
+        getPublicAds: getPublicAds,
+        edit: editAd,
+        gatAdById: gatAdById,
+        add: addAd,
+        delete: deleteAd
+    }
+}]);
