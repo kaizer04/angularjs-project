3cf72b15d869a22f3548eb97f49f792522f39fdc
kaizer04 <kaizer0505@gmail.com>
2015-01-08 20:15:11 +0200

    added isLoggedIn function

--- app/js/services/authentication.js
+++ app/js/services/authentication.js
@@ -29,11 +29,16 @@ app.factory('authentication', function(){
         return isAdmin;
     }
 
+    function isLoggedIn() {
+        return !!getUserData();
+    }
+
     return {
         saveUser: saveUserData,
         getUser: getUserData,
         getHeaders: getHeaders,
         removeUser: removeUser,
-        isAdmin: isAdmin
+        isAdmin: isAdmin,
+        isLoggedIn: isLoggedIn
     }
 });
